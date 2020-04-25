3840d84208751182d736e89a1c4bf748473dafbd
vickyuan <56779247+vickyuan@users.noreply.github.com>
2020-04-26 05:24:55 +0800

    hero2-1[list]:mock-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes-hero1';
+  title = 'Tour of Heroes-hero2';
 }
