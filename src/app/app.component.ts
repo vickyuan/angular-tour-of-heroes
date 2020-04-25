e0a3e368f908bca7d22d280fe2b022eaeef82f09
vickyuan <56779247+vickyuan@users.noreply.github.com>
2020-04-26 05:14:46 +0800

    Master:component更新title-Master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes-hero1';
+  title = 'Tour of Heroes-Master';
 }
