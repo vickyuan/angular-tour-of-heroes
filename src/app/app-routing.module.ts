1d91abe671f446c8c8d1feb47965037727fa95d2
vickyuan <56779247+vickyuan@users.noreply.github.com>
2020-04-26 17:17:42 +0800

    hero5-3[Add dashboard]router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 // 元数据会初始化路由器，并开始监听浏览器地址的变化。
 @NgModule({
