73f9cf5b478da88e163a280237bdd5b59934758b
vickyuan <56779247+vickyuan@users.noreply.github.com>
2020-04-26 12:31:46 +0800

    hero5-1[router a component in root page]

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+// 元数据会初始化路由器，并开始监听浏览器地址的变化。
+@NgModule({
+  // declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  // 导出 RouterModule，以便它在整个应用程序中生效
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
